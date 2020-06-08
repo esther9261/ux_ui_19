03811d2f79a42e84007b40ca96c555d946a6f33f
Esther Kang <58163755+esther9261@users.noreply.github.com>
2020-06-08 16:32:49 -0700

    k
    kk

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
+$(function () {
 
-$(".navItems a").click(function(){
-    $(".navItems a").toggleClass("active");
+$(".navItems a").click(function() {
+    $(".navItems a").toggleClass("active"); 
+
     if($(".navItems a").hasClass("active")){
         $(".searchBar").css("height","100vh)");
+    }
+});
   
+    $("#workLink").click(function(){
+        $(location).attr('href', '/index.html');
+    })
+
+    $("#aboutLink").click(function(){
+        $(location).attr('href', '/index2.html');
+    })
+    
+
+})
